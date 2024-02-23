173f38b9fd6241a2f4cfddf8ab2bf44da96623da
tmorio <20278135+tmorio@users.noreply.github.com>
2024-02-23 23:11:59 +0900

    スパムフィルタの条件変更

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -434,14 +434,10 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
     retry
   end
 
-  SPAM_FILTER_MINIMUM_FOLLOWERS = ENV.fetch('SPAM_FILTER_MINIMUM_FOLLOWERS', 0).to_i
-  SPAM_FILTER_MINIMUM_CREATE_DAYS = ENV.fetch('SPAM_FILTER_MINIMUM_CREATE_DAYS', 1).to_i
   SPAM_FILTER_MINIMUM_MENTIONS = ENV.fetch('SPAM_FILTER_MINIMUM_MENTIONS', 1).to_i
   def like_a_spam?
     (
       !@status.account.local? &&
-      @status.account.followers_count <= SPAM_FILTER_MINIMUM_FOLLOWERS &&
-      @status.account.created_at > SPAM_FILTER_MINIMUM_CREATE_DAYS.day.ago &&
       @mentions.count > SPAM_FILTER_MINIMUM_MENTIONS
     )
   end
